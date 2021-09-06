540ca5f8bbc32ac0e33167b8dc07b6158826d3c5
JCJ05 <julitoaguerorengifo@hotmail.com>
2021-09-06 09:50:22 -0500

    Panel de Trabajo

---
+++ Primer-Ejemplo/spring-boot-web/src/test/java/com/ejemplo/spring/springbootweb/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.spring.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
